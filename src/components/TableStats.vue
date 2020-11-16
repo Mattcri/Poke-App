e9b6ba0a47ee1f2ac63c030a464fc6f4e0fd5dac
Mcriado <mcriado.cg@gmail.com>
2020-11-15 23:24:13 -0300

    Avances en detail

---
+++ src/components/TableStats.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-simple-table >
+    <template v-slot:default >
+      <thead>
+        <tr >
+          <th class="text-left">
+            HP
+          </th>
+          <th class="text-left">
+            Attack
+          </th>
+          <th class="text-left">
+            Defense
+          </th>
+          <th class="text-left">
+            Special-attack
+          </th>
+          <th class="text-left">
+            Special-defense
+          </th>
+          <th class="text-left">
+            Speed
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in stats" :key="index">
+          <td>{{ item.base_stats }}</td>
+          <!-- <td>{{ item.calories }}</td> -->
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+  
+</template>
+
+<script>
+export default {
+  name: 'TableStats',
+  props: {
+    stats: Array
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/PokeDetail.vue
+++ src/views/PokeDetail.vue
@@ -1,38 +1,83 @@
 <template>
   <v-container v-if="pokemonDetail">
-    <h2>Detail Poke Page: {{pokemonDetail.name}}</h2>
+
+    <h2 :class="'capitalize'" class="my-2">Nombre: {{pokemonDetail.name}}</h2>
     <v-divider></v-divider>
-    <span>{{pokemonDetail.sprites.back_default}}</span>
-    <v-img
-      :src="pokemonDetail.sprites.back_default"
-      max-width="200"
-    ></v-img>
-    <v-img
-      :src="pokemonDetail.sprites.front_shiny"
-      max-width="200"
-    ></v-img>
-    <v-img
-      :src="pokemonDetail.sprites.front_default"
-      max-width="200"
-    ></v-img>
-    <v-img
-      :src="pokemonDetail.sprites.back_shiny"
-      max-width="200"
-    ></v-img>
+
+    <v-row class="mb-4">
+      <v-col cols="12" md="6" class="d-flex justify-center" >
+        <v-img
+          height="220"
+          max-width="200"
+          class="blue-grey lighten-4 rounded-circle mt-4"
+          contain
+          :src="pokemonDetail.sprites.front_default"
+          alt="Pokemón"
+          transition="fab-transition"
+          :class="'shadow'"
+        >
+        </v-img>
+
+      </v-col>
+      <v-col cols="12" md="6" class="d-flex justify-center">
+        <v-img
+          height="220"
+          max-width="200"
+          class="blue-grey lighten-4 rounded-circle mt-4"
+          contain
+          :src="pokemonDetail.sprites.back_default"
+          alt="Pokemón"
+          transition="fab-transition"
+          :class="'shadow'"
+        >
+        </v-img>
+
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+
+    <h2 :class="'capitalize'" class="my-3">caracteristicas</h2>
+    <p>Altura: <strong class="purple--text lighten-1">{{ pokemonDetail.height }} pulgadas</strong> </p>
+    <p>Peso: <strong class="purple--text lighten-1">{{ pokemonDetail.weight }} libras</strong></p>
+
+    <span>{{pokeStats}}</span>
+    <br>
+    <span>{{statsMap}}</span>
+    <br>
+    <p>{{test}}</p>
+    <div v-for="(s, index) in PokeDetail" :key="index">
+      <p>{{s.base_stat}}</p>
+      <br>
+      <p>{{s.stat.name}}</p>
+      <!-- <p>{{s.base_stat}}</p> -->
+    </div>
+
+    <table-stats :stats="pokeStats"  />
     
   </v-container>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import TableStats from '@/components/TableStats.vue'
+
 
 export default {
   name: 'PokeDetail',
+  components: { TableStats },
   computed: {
     ...mapState(['pokemonDetail']),
-    // pokeName () {
-    //   return this.pokemonDetail.name
-    // }
+    pokeStats () {
+      return this.pokemonDetail.stats.map(item => {
+        return item.base_stat
+      })
+    },
+    test () {
+      return this.pokemonDetail.stats.map(item => Object.entries(item.base_stat))
+    },
+    statsMap () {
+      return this.pokemonDetail.stats.map(item => item)
+    }
     // pokeImg () {
     //   return this.pokemonDetail.sprites.back_default
     // }
@@ -41,6 +86,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.capitalize {
+    text-transform: capitalize;
+    text-align: center;
+}
+.shadow {
+  box-shadow: 0px 2px 3px 3px rgba(0,0,0,0.45)
+}
 
 </style>
